ec26315f62172e7393b93562994eb634d2387223
Perla Ordonez <guate.po@gmail.com>
2019-01-20 12:43:52 -0600

    added code to logic.js

---
+++ assets/logic.js
@@ -0,0 +1,53 @@
+var config = {
+    apiKey: "AIzaSyDUYr4bUBa1ehEReyR-dQsXNCyy7rJSUiA",
+    authDomain: "train-scheduler-1ef20.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1ef20.firebaseio.com",
+    projectId: "train-scheduler-1ef20",
+    storageBucket: "train-scheduler-1ef20.appspot.com",
+    messagingSenderId: "632221294811"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  $("#submit").on("click", function(event){
+        event.preventDefault();
+
+        var trnName = $("#train-input").val().trim();
+        var trnDestination= $("#destination-input").val().trim();
+        var trnStart = moment($("#start-input").val().trim(), "HH:mm").format("HH:mm");
+        var trnFrequency = $("#frequency-input").val().trim();
+
+        var newTrain = {
+            name: trnName,
+            destination: trnDestination,
+            start: trnStart,
+            frequency: trnFrequency
+        };
+        database.ref().push(newTrain);
+
+        $("#train-input").val("");
+        $("#destination-input").val("");
+        $("#start-input").val("");
+        $("#frequency-input").val("");
+  });
+
+database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val());
+
+    var trnName = childSnapshot.val().name;
+    var trnDestination = childSnapshot.val().destination;
+    var trnStart = childSnapshot.val().start;
+    var trnFrequency = childSnapshot.val().frequency;
+    // var nxtArrival=
+    // var minAway=
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trnName),
+        $("<td>").text(trnDestination),
+        $("<td>").text(trnFrequency),
+        // $("<td>").text(nxtArrival),
+        // $("<td>").text(minAway),
+    );
+    $("#train-table > tbody").append(newRow);
+});
